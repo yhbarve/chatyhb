4aa83ef28d7a06d87d69cd86109d8b05a21acbf0
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-06-27 19:08:32 -0400

    adding Link instead of <a>

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar(){
     return (
         <div className="bg-teal-500 py-5 px-6 xl:px-48 flex flex-wrap gap-4 justify-center md:justify-between items-center fixed top-0 left-0 right-0">
             <div className="text-4xl font-bold text-white">ChatYHB</div>
             <div className="flex justify-center gap-12">
-                <a href="/login" className="bg-white p-2 px-5 text-red-500 font-medium rounded hover:bg-red-500 hover:text-white transition ease-in">Login</a>
-                <a href="/signup" className="bg-white p-2 px-5 text-red-500 font-medium rounded hover:bg-red-500 hover:text-white transition ease-in">Sign Up</a>
+                <Link to="/login" className="bg-white p-2 px-5 text-red-500 font-medium rounded hover:bg-red-500 hover:text-white transition ease-in">Login</Link>
+                <Link to="/signup" className="bg-white p-2 px-5 text-red-500 font-medium rounded hover:bg-red-500 hover:text-white transition ease-in">Sign Up</Link>
             </div>
         </div>
     )
