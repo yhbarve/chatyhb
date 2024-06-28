05541257c3517dd38d3dcd23aa222b7b3ac56aaa
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-06-27 22:10:00 -0400

    checking api key

--- backend/routes/request.js
+++ backend/routes/request.js
@@ -13,6 +13,7 @@ const API_KEY=process.env.API_KEY;
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+console.log(API_KEY);
 
 async function run(prompt) {
     const result = await model.generateContent(prompt);
